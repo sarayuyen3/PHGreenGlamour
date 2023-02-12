17331ce396b11a8ae5c747601effe27dfdac8d4d
Sarayu Yenumula <sarayu@ad.unc.edu>
2023-02-12 05:02:17 -0500

    Initial Commit

---
+++ PHGreenGlamour/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  PHGreenGlamour
+//
+//  Created by Sarayu Yenumula on 2/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var link: String = ""
+    @State private var highlights: [String] = []
+    @State private var ingredients: [String] = []
+    @State private var laneige: [String] = []
+    
+    var body: some View {
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [Color(red: 1.0, green: 0.922, blue: 0.929), Color(red: 0.9961, green: 0.8392, blue: 0.8588), Color(red: 0.9800, green: 0.8300, blue: 0.8500)]), startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            VStack {
+                Text("green glamour")
+                    .font(.system(.title, design: .rounded, weight: .semibold))
+                    .foregroundColor(Color(red: 0.3412, green: 0.4588, blue: 0.4039))
+                TextField("Enter the link", text: $link)
+                    .padding()
+                Button(action: {
+                    self.laneige = ["Tatcha", "The Dewy Skin Cream Plumping & Hydrating Moisturizer","$70.00","Hyaluronic Acid, Community Favorite, Plumping, Hydrating, Clean at Sephora, Good for: Dryness", "This product is cruelty-free, and gluten-free.", "Although the sustainable option we recommend may appear to have a higher upfront cost than the original product, it is important to consider the long-term value and environmental benefits it provides."]
+                }) {
+                    Text("Find a sustainable product!")
+                        .foregroundColor(Color(red: 0.5137, green: 0.6392, blue: 0.5804))
+                        
+                }
+                if link == "https://www.sephora.com/product/cream-skin-toner-moisturizer-P446930?skuId=2258812&icid2=products%20grid:p446930:product"
+                {
+                    VStack(alignment: .leading) {
+                        Text("Laneige Alternative:")
+                            .font(.headline)
+                            .padding()
+                        ForEach(laneige, id: \.self) { laneige in
+                            Text("✔" + laneige)
+                                .font(.footnote)
+                                .padding()
+                        }
+                    }
+                }
+                
+                }
+            }
+        }
+    }
---
+++ PHGreenGlamour/PHGreenGlamourApp.swift
@@ -0,0 +1,17 @@
+//
+//  PHGreenGlamourApp.swift
+//  PHGreenGlamour
+//
+//  Created by Sarayu Yenumula on 2/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PHGreenGlamourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
